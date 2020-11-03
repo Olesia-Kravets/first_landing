c4fc75adedc0975a0cb95e4ed3a30764a01278ae
OlesyaChu <olesya.kravets@gmail.com>
2020-11-03 13:00:50 +0300

    fix main.js

--- js/main.js
+++ js/main.js
@@ -10,31 +10,31 @@ const btnNextMobile = document.getElementById('btn_next');
 let index = 0;
 
 
-const activeSlide = n => {
+const setActiveSlide = n => {
     for(let slide of slides){
         slide.classList.remove('active');
-        slides[n].classList.add('active');
     }
+    slides[n].classList.add('active');
 }
 
-const activeLink = n => {
+const setActiveLink = n => {
     for(let link of sliderLinks){
         link.classList.remove('active');
-        sliderLinks[n].classList.add('active');
     }
+    sliderLinks[n].classList.add('active');
 }
 
-const activeDot = n => {
+const setActiveDot = n => {
     for(let dot of dots){
         dot.classList.remove('active');
-        dots[n].classList.add('active');
     }
+    dots[n].classList.add('active');
 }
 
 const showSlide = (i) => {
-    activeSlide(i);
-    activeLink(i);
-    activeDot(i);
+    setActiveSlide(i);
+    setActiveLink(i);
+    setActiveDot(i);
 }
 
 const nextSlide = () => {
@@ -74,27 +74,32 @@ sliderLinks.forEach((itemLink, indexLink) => {
 arrowNext.addEventListener('click', nextSlide);
 arrowPrev.addEventListener('click', prevSlide);
 
+btnNextMobile.addEventListener('click', nextSlide);
+btnPrevMobile.addEventListener('click', prevSlide);
+
+
+
 // для экранов max-width: 575px 
 
-const nextSlideMobile = () => {
-    if(index === slides.length - 1) {
-        index = 0;
-        activeSlide(index);
-    } else {
-        index ++;
-        activeSlide(index);
-    }
-}
+// const nextSlideMobile = () => {
+//     if(index === slides.length - 1) {
+//         index = 0;
+//         setActiveSlide(index);
+//     } else {
+//         index ++;
+//         setActiveSlide(index);
+//     }
+// }
 
-const prevSlideMobile = () => {
-    if(index === 0) {
-        index = slides.length - 1;
-        activeSlide(index);
-    } else {
-        index --;
-        activeSlide(index);
-    }
-}
+// const prevSlideMobile = () => {
+//     if(index === 0) {
+//         index = slides.length - 1;
+//         setActiveSlide(index);
+//     } else {
+//         index --;
+//         setActiveSlide(index);
+//     }
+// }
 
-btnNextMobile.addEventListener('click', nextSlideMobile);
-btnPrevMobile.addEventListener('click', prevSlideMobile);
+// btnNextMobile.addEventListener('click', nextSlideMobile);
+// btnPrevMobile.addEventListener('click', prevSlideMobile);
